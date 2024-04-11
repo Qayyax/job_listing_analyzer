b5030ed936bfe8a786604f8d5472f388acd8a994
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-04-11 13:28:07 -0400

    finished pagination, added comments

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ title = input("Enter your desired job title: ")
 city = input("Enter your desired location: ")
 
 
-def get_job_url(title, city):
+def get_job_url(title: str, city: str) -> str:
+    """This gets the user input and returns a url to the main page"""
     title = title.replace(" ", "+")
     city = city.replace(" ", "+")
     url = f"https://www.workopolis.com/search?q={title}&l={city}"
@@ -20,6 +21,7 @@ def get_job_url(title, city):
 
 
 def get_html(url):
+    """This parses the html from the url entered."""
     headers = {
         "Dnt": "1",
         "Referer": "https://www.workopolis.com/search?q=front+end+developer&l=toronto",
@@ -33,23 +35,19 @@ def get_html(url):
     return doc
 
 
-doc = get_html(get_job_url(title, city))
-total_job = int(doc.find(class_="chakra-text css-gu0het").text)
-job_div = doc.find("ul", {"id": "job-list"})
-job_href = job_div.find_all(class_="chakra-button css-27ga1i")
-page_nav = doc.find("nav", {"role": "navigation", "class": "css-1hog1e3"})
-page_nav_link = page_nav.find_all("a", {"class": "chakra-link css-kqypvt"})
-next_page = page_nav.find("a", {"aria-label": "Next page"})
-# print(next_page)
-
-for page in page_nav_link:
-    pass
-    # print(page)
-# print(total_job)
+def get_jobs_on_page(page):
+    """
+    This returns the Beatifulsoup dict that contains all the job on a page"""
+    job_div = page.find("ul", {"id": "job-list"})
+    job_href = job_div.find_all(class_="chakra-button css-27ga1i")
+    return job_href
 
 
-def get_job_dict(url: dict) -> dict:
-    job_links = 'https://www.workopolis.com' + url['href']
+def get_job_dict(job_url):
+    """
+    This function returns a dictionary of the job details.
+    """
+    job_links = 'https://www.workopolis.com' + job_url['href']
     job_html = get_html(job_links)
     job_title = job_html.find('h1', {"class": "chakra-heading css-yvgnf2"}).text
     job_company = job_html.find('span', {"data-testid": "viewJobCompanyName"}).text
@@ -75,5 +73,20 @@ def get_job_dict(url: dict) -> dict:
     return details
 
 
-for job in job_href:
-    print(get_job_dict(job))
+doc = get_html(get_job_url(title, city))
+jobs_found = []
+
+while True:
+    page_nav = doc.find("nav", {"role": "navigation", "class": "css-1hog1e3"})
+    jobs = get_jobs_on_page(doc)
+    for job in jobs:
+        job_found = get_job_dict(job)
+        jobs_found.append(job_found)
+        print(job_found)
+    next_page = page_nav.find("a", {"aria-label": "Next page"})
+    if next_page:
+        doc = get_html(next_page['href'])
+    else:
+        break
+    if len(jobs_found) >= 1000:
+        break
